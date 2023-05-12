895fba62fc4f2e4a9e2c37a297c25dcda25d9463
mdarlow <mdarlow@gmail.com>
2023-05-11 22:59:29 -0700

    (pc1) Add images

--- src/main.py
+++ src/main.py
@@ -1,20 +1,50 @@
 import pygame
 import os
 
+#################
+# Display setup #
+#################
 pygame.init()
 # FULLSCREEN MODE:
 # pygame.display.set_mode((0,0),pygame.FULLSCREEN)
 #WINDOWED MODE:
 WIDTH, HEIGHT = (500, 500)
-pygame.display.set_mode((HEIGHT, WIDTH),pygame.RESIZABLE)
+win = pygame.display.set_mode((HEIGHT, WIDTH),pygame.RESIZABLE)
 pygame.display.set_caption("Hangman")
 
+###############
+# Load images #
+###############
+images = [] # all images
+for i in range(7):
+    image = pygame.image.load("src/images/hangman" + str(i) + ".png") # get name of an image
+    images.append(image) # add each image name
+
+############################################################################################## Create img with surface of 209x216x32
+
+#############
+# variables #
+#############
+hangman_status = 0
+
+##########
+# Colors #
+##########
+WHITE = (255, 255, 255)
+
+###################
+# Game loop setup #
+###################
 FPS = 60
 clock = pygame.time.Clock()
 run = True
 
 while run:
     clock.tick(FPS) # game runs at steady speed
+
+    win.fill((WHITE))
+    win.blit(images[hangman_status], (150, 100)) # blit(images) = draw images
+    pygame.display.update() # update display with filling color
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
